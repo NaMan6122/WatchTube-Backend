8cfe958be7cc43a7c311c9a35a49d9306a1e2426
NaMan6122 <namang2510@gmail.com>
2024-07-27 02:38:52 +0530

    User and Video schema, bcrypt, jwt

---
+++ src/models/userModel.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userModel = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true, //property to make searching optimized and fast in MongoDB.
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    fullname: {
+        type: String,
+        required: true, 
+        trim: true,
+    },
+    avatar: {
+        type: String, //cloudinary url.
+        required: true,
+    },
+    coverImage: {
+        type: String, //cloudinary url.
+    },
+    watchHistory: {
+        type: Array,
+        default: [],
+    },
+    refreshToken: {
+        type: String,
+
+    }
+}, {timestamps : true});
+
+//now hashing and storing password:
+//pre is a hook in mongoose whose functionality is to modify any passed Schema data such as password encryption,
+//just before it is saved to the database(thats what a hook means!).
+
+userModel.pre("save", async function (next) {
+    if(this.isModified("password")){ //checking if the password field is modified or not.
+        //modifying this.password, this here refers to the information ectracted by the pre hook.
+        this.password = await bcrypt.hash(this.password, 10); 
+    }
+});
+
+//injecting a custom method, feature of mongoose.
+userModel.methods.isPasswordCorrect = async () => {
+    return await bcrypt.compare(password, this.password);
+    //password : raw new password entered by the user.
+    //this.password : hashed password stored in the database.
+}
+
+//generating tokens using jwt by injecting custom methods in schema.
+userModel.methods.generateAccessToken = async () => {
+    const token = jwt.sign({
+        _id: this._id.toString(),
+        email: this.email,
+        fullName: this.fullName,
+        username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    });
+    return token;
+}
+
+userModel.methods.generateRefreshToken = async () => {
+    const token = jwt.sign({
+        _id: this._id.toString(),
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    });
+    return token;
+}
+
+export const User = mongooose.Model("User", userModel);
---
+++ src/models/videoModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoModel = new mongoose.Schema({
+    videoFile: {
+        type: String, //cloudinary url.
+        required: true,
+    },
+    thumbnail: {
+        type: String, //cloudinary url.
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number, //from cloudinary properties.
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+}, {timestamps: true});
+
+//aggregation queries can be written now[we are using this to handle and process the watch history field**].
+videoModel.plugin(mongooseAggregatePaginate);
+
+//Aggregation queries in MongoDB are used to process data records and return computed results. 
+//These queries allow for operations such as filtering, grouping, sorting, and reshaping documents within a collection.
+//The aggregation framework operates through an aggregation pipeline,
+// where each stage transforms the documents in some way and passes the results to the next stage.
+
+//Pagination is used to divide a large dataset into smaller, manageable chunks (pages). 
+//Pagination allows you to fetch a subset of the results, 
+//which is particularly useful for displaying data in a user interface.
+
+export const Video = mongoose.Model("Video", videoModel);
