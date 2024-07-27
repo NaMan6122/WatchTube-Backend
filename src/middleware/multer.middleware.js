34f86953261e2a429dc3471f0e6640510615fe27
NaMan6122 <namang2510@gmail.com>
2024-07-28 01:43:27 +0530

    Cloudinary config, Multer Middleware Setup

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer"
+
+//configuring multer, storing file on disk and not on memory, read multer documentation.
+const storage = multer.diskStorage({
+    //cb is callback function () => {} here.
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        //adding a randomized suffix in the original filename for security purposes and naming conflicts.
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  });
+  
+  //creating the upload variable using multer function:
+  //The multer function expects an object with configuration options, not the storage object itself. 
+export const upload = multer({storage : storage});
---
+++ src/models/cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from cloudinary;
+import fs from "fs"; //Node.js file system module.
+
+//setting up cloudinary.
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        //uploading the file if it exists.
+        const response = await cloudinary.uploader.upload("https://images.app.goo.gl/ntKhKQm2a4q7DQ1x9", {
+            resource_type: "auto",
+        });
+        console.log("File is uploaded!")
+        console.log(response, response.url);// response.url is the cloudinary url.
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //removing the file from the database, as the operation failed.
+        return null;
+    }
+}
+
+// documentation method:
+// cloudinary.v2.uploader.upload("https://images.app.goo.gl/ntKhKQm2a4q7DQ1x9",
+//     {
+//         public_id: "scenary_pic"
+//     },
+//     function(error, result){
+//         console.log(result);
+//     }
+// );
